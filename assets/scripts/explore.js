e16c5b1f6dfc6fbe3574e5fa636d0540a467867a
vumary <vuumary@gmail.com>
2023-05-07 21:10:55 -0700

    testing speechSynthesis.getVoices()

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,20 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  // TODO 
+  console.log("hi");
+  const voices = speechSynthesis.getVoices();
+  console.log(voices);
+  for (let i = 0; i < voices.length; i++) {
+    const option = document.createElement("option");
+    option.textContent = `${voices[i].name} (${voices[i].lang})`;
+    console.log("hi");
+    if (voices[i].default) {
+      option.textContent += " — DEFAULT";
+    }
+
+    option.setAttribute("data-lang", voices[i].lang);
+    option.setAttribute("data-name", voices[i].name);
+    document.getElementById("voice").appendChild(option);
+  }
 }
