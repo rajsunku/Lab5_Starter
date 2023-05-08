7365ee953c64ccc5dee7590417e1971f62c8d7e9
vumary <vuumary@gmail.com>
2023-05-07 21:48:18 -0700

    completed explore.js

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,19 +4,59 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO 
-  console.log("hi");
-  const voices = speechSynthesis.getVoices();
-  console.log(voices);
-  for (let i = 0; i < voices.length; i++) {
-    const option = document.createElement("option");
-    option.textContent = `${voices[i].name} (${voices[i].lang})`;
-    console.log("hi");
-    if (voices[i].default) {
-      option.textContent += " — DEFAULT";
+  const synth = window.speechSynthesis;
+  const voiceSelect = document.querySelector("select");
+  const inputTxt = document.getElementById("text-to-speak");
+  const button = document.querySelector("button");
+  const changeImg = document.querySelector("img");
+  let voices = [];
+
+  function populateVoiceList() {
+    voices = synth.getVoices();
+  
+    for (let i = 0; i < voices.length; i++) {
+      const option = document.createElement("option");
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+  
+      if (voices[i].default) {
+        option.textContent += " — DEFAULT";
+      }
+  
+      option.setAttribute("data-lang", voices[i].lang);
+      option.setAttribute("data-name", voices[i].name);
+      voiceSelect.appendChild(option);
+    }
+  }
+
+  populateVoiceList();
+  if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = populateVoiceList;
+  }
+
+  button.onclick = (event) => {
+    event.preventDefault();
+  
+    const utterThis = new SpeechSynthesisUtterance(inputTxt.value);
+    const selectedOption =
+      voiceSelect.selectedOptions[0].getAttribute("data-name");
+    for (let i = 0; i < voices.length; i++) {
+      if (voices[i].name === selectedOption) {
+        utterThis.voice = voices[i];
+      }
     }
 
-    option.setAttribute("data-lang", voices[i].lang);
-    option.setAttribute("data-name", voices[i].name);
-    document.getElementById("voice").appendChild(option);
+    synth.speak(utterThis);
+
+    if (synth.speaking) {
+      console.log("test");
+      changeImg.src = "assets/images/smiling-open.png";
+    } 
+
+    utterThis.addEventListener("end", (event) => {
+      changeImg.src = "assets/images/smiling.png";
+    });
+
   }
+
 }
+
